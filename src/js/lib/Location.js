663bd1884110cfee290125689547c251c2408b6e
pasquale <pasqualemangialavori@buongiorno.com>
2017-01-10 12:32:50 +0100

    isProduction as function in Location

--- src/js/lib/Location.js
+++ src/js/lib/Location.js
@@ -1,13 +1,14 @@
 import { Utils } from 'stargatejs';
 let theWindow = {};
+const isProduction = () => process.env.NODE_ENV !== "production";
 
-if(process.env.NODE_ENV !== "production"){
+if(isProduction()){
     let DEBUG_OPTIONS = {};
     let windowConf = require('./windowConf');
 
     DEBUG_OPTIONS = Utils.dequeryfy(window.location.href);
     theWindow.location = windowConf(DEBUG_OPTIONS.host, DEBUG_OPTIONS.game_id, DEBUG_OPTIONS.country_code);
-} else if(process.env.NODE_ENV === "production") {
+} else if(!isProduction()) {
     theWindow = window;
 }
 
