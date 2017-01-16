f7d72e21c985ad16cbef6fa9735df8543605b4e9
pasquale <pasqualemangialavori@buongiorno.com>
2017-01-16 09:27:25 +0100

    banner and button updated

--- src/js/components/Banner.js
+++ src/js/components/Banner.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button } from './Button';
+import genericStyle from '../../css/generic.css';
 import bannerStyle from '../../css/banner.css';
 
 /** Connect to redux store */
@@ -9,7 +10,15 @@ import { bindActionCreators } from 'redux';
 /** TODO: import only needed actions */
 import { Actions } from '../actions/index';
 const mapStateToProps = (state) => {
-  return {...state.banner}
+  return {
+      ...state.banner,
+      dictionary: {
+          WEB_INSTALL_BUTTON_TEXT: 'Get the app',
+          BANNER_DESCRIPTION: `Get the Native app
+          Get the app to increase your Game Experience!
+          ...and play OFFLINE too!`
+      }
+    }
 };
 
 const mapDispatchToProps = (dispatch) => ({
@@ -22,6 +31,7 @@ export class Banner extends React.Component {
     constructor(props){
         super(props);
         this.handleClose = this.handleClose.bind(this);
+        this.handleGetAppButton = this.handleGetAppButton.bind(this);
     }
 
     handleClose(evt){
@@ -29,18 +39,43 @@ export class Banner extends React.Component {
         this.props.actions.hideBanner();
     }
 
+    handleGetAppButton(evt){
+        evt.preventDefault();
+        this.props.actions.redirectOnStore();
+    }
+
     render(){
         let show = this.props.show ? bannerStyle.show : '';
         let contanierClass = [bannerStyle.bannerContainer, show].join(' ');
-        const classNames = bannerStyle.banner;
-        return (
+        return (            
             <div className={contanierClass}>
-                <div className={classNames}>
-                    <button className={bannerStyle.closeButton} onClick={this.handleClose}>
-                        <span >
-                        </span>
-                    </button>
-                    <Button text='Get the app' />
+                <div className={bannerStyle.banner}>
+
+                    <div className={genericStyle.grid}>
+                        
+                        <div className={genericStyle.col + ' ' + genericStyle._10_12}></div>
+                        <div className={genericStyle.col + ' ' + genericStyle._2_12}>
+                             <button className={bannerStyle.closeButton} onClick={this.handleClose}>
+                                <span></span>
+                            </button>
+                        </div>
+                        <div className={genericStyle.col + ' ' + genericStyle._1_12}></div>
+
+
+                            <div className={genericStyle.col + ' ' + genericStyle._2_12} style={{marginTop:'10px'}}>
+                                <div className={bannerStyle.appIcon}></div>
+                            </div>
+                            <div className={genericStyle.col + ' ' + genericStyle._9_12} style={{marginTop:'10px'}}>
+                                <p className={bannerStyle.appDescription}>
+                                    {this.props.dictionary.BANNER_DESCRIPTION}
+                                </p>
+                            </div>
+                        <div className={genericStyle.col + ' ' + genericStyle._3_12}></div>
+                        <div className={genericStyle.col + ' ' + genericStyle._6_12} style={{padding:'0',marginTop:'25px'}}>
+                            <Button text={this.props.dictionary.WEB_INSTALL_BUTTON_TEXT} style={{fontSize: '15px'}} onClick={this.handleGetAppButton}/>
+                        </div>
+                        <div className={genericStyle.col + ' ' + genericStyle._3_12}></div>
+                    </div>                    
                 </div>
             </div>
         )
--- src/js/components/Button.js
+++ src/js/components/Button.js
@@ -4,12 +4,38 @@ import buttonStyle from '../../css/button.gameasy.css';
 export class Button extends React.Component {
     constructor(props){
         super(props);
+        this.state = {
+            active: false
+        }
+        this.onMouseDown = this.onMouseDown.bind(this);
+        this.onMouseUp = this.onMouseUp.bind(this);
+    }
+
+    onMouseDown(evt){
+        evt.preventDefault();
+        this.setState({ active: true });
+    }
+
+    onMouseUp(evt){
+        evt.preventDefault();
+        this.setState({ active: false });
+        this.props.onClick(evt);
     }
 
     render(){
-        let classNames = [buttonStyle.btnGameasy, buttonStyle.centerBtn].join(' ');
+        let buttonClasses = [buttonStyle.btnGameasy];        
+        if(this.props.center){ buttonClasses.push(buttonStyle.centerBtn); }
+        if(this.state.active){ buttonClasses.push(buttonStyle.active); }
+
+        let classNames = buttonClasses.join(' ');
         return(
-            <button onClick={this.props.onClick} className={classNames}>{this.props.text}</button>
+            <button 
+                onMouseDown={this.onMouseDown} 
+                onMouseUp={this.onMouseUp} 
+                className={classNames} 
+                style={{...this.props.style}}>
+                {this.props.text}
+            </button>
         )
     }
 }
