64c94504d443edcbdbcf4c9dee14b21c06c51524
eatsjobs <pasquale.mangialavori@gmail.com>
2017-01-21 19:10:35 +0100

    first debug implementation

--- webpack.config.debug.js
+++ webpack.config.debug.js
@@ -7,6 +7,25 @@ var devConfiguration = Object.create(baseConfiguration);
 var optimizeOccurence = new webpack.optimize.OccurenceOrderPlugin();
 var env_Plugin = new webpack.DefinePlugin({ 'process.env': { 'NODE_ENV': JSON.stringify('debug') } });
 
+devConfiguration.devServer = { 
+    contentBase: 'debug/',
+    host: 'local.appsworld.gamifive-app.com',
+    proxy: {
+      '/v01/**': {
+        target: 'http://www2.gameasy.com/ww-it/',
+        secure: false,
+        changeOrigin: true,
+        historyApiFallback: true,
+        pathRewrite: {
+          '': ''
+        },
+        cookieDomainRewrite:{'*':''}
+      }
+    }
+};
+
+
+devConfiguration.output.path = path.resolve(__dirname, "debug");
 devConfiguration.output.filename = 'gfsdk.debug.js';
 devConfiguration.devtool = 'eval-source-map';
 devConfiguration.plugins.push(optimizeOccurence);
