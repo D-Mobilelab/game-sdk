5a5802e3055d7f55da4da153a7a24612e7b21f9f
pasquale <pasqualemangialavori@buongiorno.com>
2017-01-30 14:30:44 +0100

    eslint for jasmine

--- src/js/actions/__tests__/user-actions-test.js
+++ src/js/actions/__tests__/user-actions-test.js
@@ -3,7 +3,7 @@ import thunk from 'redux-thunk';
 import * as userActions from '../user-actions';
 import { USER_CHECK,
          USER_SET_LIKE,
-         USER_GET_LIKE, 
+         USER_GET_LIKE,
          USER_DELETE_LIKE } from '../../lib/Constants';
 
 require('jasmine-ajax');
@@ -11,27 +11,26 @@ require('jasmine-ajax');
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 
-// This sets the mock adapter on the default instance
-describe('async actions', function(){
+describe('User', () =>  {
 
-  beforeEach(function() {
+  beforeEach(() => {
     jasmine.Ajax.install();
-  });  
+  });
 
-  afterEach(function() {
+  afterEach(() => {
     jasmine.Ajax.uninstall();
   });
 
-  it('user favourites action', function(done){
-    let poggioId = "903833c2c35a11e589cb005056b60712";
-    let url = USER_GET_LIKE + '?user_id=' + poggioId + '&size=51';
+  it('should get the favourites when 200', (done) =>  {
+    const poggioId = '903833c2c35a11e589cb005056b60712';
+    const url = `${USER_GET_LIKE}?user_id=${poggioId}&size=51`;
 
-      jasmine.Ajax.stubRequest(url).andReturn({
-        "status": 200,
-        "contentType": 'text/plain',
-        "responseText": '[]',
-        response: []
-      });
+    jasmine.Ajax.stubRequest(url).andReturn({
+      status: 200,
+      contentType: 'text/plain',
+      responseText: '[]',
+      response: [],
+    });
 
     const store = mockStore({
       user: {
@@ -41,15 +40,15 @@ describe('async actions', function(){
       },
       generic: {
         hybrid: false,
-        connectionState: { online: true }
-      }
+        connectionState: { online: true },
+      },
     });
 
     const expectedActions = [
       { type: 'GET_FAVOURITES_START' },
-      { type: 'GET_FAVOURITES_END', favourites: [] }
+      { type: 'GET_FAVOURITES_END', favourites: [] },
     ];
-    
+
     store.dispatch(userActions.getUserFavourites())
       .then(() => {
         expect(store.getActions()).toEqual(expectedActions);
