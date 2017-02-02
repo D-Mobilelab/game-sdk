987e5e40f42f53173c357f78844277d147dfa073
pasquale <pasqualemangialavori@buongiorno.com>
2017-02-02 15:47:58 +0100

    set rank action in reducer

--- src/js/reducers/session.js
+++ src/js/reducers/session.js
@@ -1,10 +1,12 @@
-export function session(state = { opened: false }, action){
-    switch(action.type){
-        case 'START_SESSION':
-            return Object.assign({}, state, {...action.session });
-        case 'END_SESSION':
-            return Object.assign({}, state, { ...action.session });
-        default:
-            return state;
-    }
+export function session(state = { opened: false }, action) {
+  switch (action.type) {
+    case 'START_SESSION':
+      return Object.assign({}, state, { ...action.session });
+    case 'END_SESSION':
+      return Object.assign({}, state, { ...action.session });
+    case 'SET_RANK':
+      return Object.assign({}, state, { ...action.session, rank: action.rank });
+    default:
+      return state;
+  }
 }
