25d239cd8a588527dc68a24b1ba81363b3c20b25
eatsjobs <pasqualemangialavori@buongiorno.com>
2017-03-01 10:05:15 +0100

    Linting for Platform.js

--- src/js/lib/Platform.js
+++ src/js/lib/Platform.js
@@ -1,54 +1,51 @@
 import platform from 'platform';
 
 let _platform = platform;
-let setMock = () => {}
-let isTesting = () => process.env.NODE_ENV === 'testing';
-if (isTesting()){	
-	setMock = (UAString)=>{
-		_platform = platform.parse(UAString)
-	}
+let setMock = () => {};
+const isTesting = () => process.env.NODE_ENV === 'testing';
+if (isTesting()) {
+  setMock = (UAString) => {
+    _platform = platform.parse(UAString);
+  };
 }
 
 /**
  * Returns true if the browser is running on Android device
  * @returns {Boolean} - true if it's android
  */
-const isAndroid = ()=>{
-	let os = _platform.os.family;
-	if(os && typeof os === 'string'){
-		os = os.toLowerCase();
-		return os.indexOf('android') > -1;
-	} else {
-		return false;
-	}
-}
+const isAndroid = () => {
+  let os = _platform.os.family;
+  if (os && typeof os === 'string') {
+    os = os.toLowerCase();
+    return os.indexOf('android') > -1;
+  }
+  return false;
+};
 
 /**
  * Returns true if the browser is running on iOS devices
  * @returns {Boolean} - true if it's ios
  */
-const isIOS = ()=>{
-	let os = _platform.os.family;
-	if(os && typeof os === 'string'){
-		os = os.toLowerCase();
-		return os.indexOf('ios') > -1;
-	} else {
-		return false;
-	}
-}
+const isIOS = () => {
+  let os = _platform.os.family;
+  if (os && typeof os === 'string') {
+    os = os.toLowerCase();
+    return os.indexOf('ios') > -1;
+  }
+  return false;
+};
 
 /**
  * Returns true if the browser is running on macOSX system
  * it's not equal to isIOS
  * @returns {Boolean} - true if it's macOSX
  */
-const isMacOS = ()=>{
-	let os = _platform.os.family;
-	if(os && typeof os === 'string'){
-		os = os.toLowerCase();
-		return os.indexOf('os x') > -1;
-	} else {
-		return false;
-	}
-}
-export {isIOS, isAndroid, isMacOS, setMock};
+const isMacOS = () => {
+  let os = _platform.os.family;
+  if (os && typeof os === 'string') {
+    os = os.toLowerCase();
+    return os.indexOf('os x') > -1;
+  }
+  return false;
+};
+export { isIOS, isAndroid, isMacOS, setMock };
