8e5af6f3815ec03aee6fdea9d12cef927fb40480
eatsjobs <pasqualemangialavori@buongiorno.com>
2017-03-23 17:10:55 +0100

    windowConf only for dev

--- src/js/lib/windowConf.js
+++ src/js/lib/windowConf.js
@@ -1,13 +1,34 @@
-export default function location({ HOST = 'appsworld.gamifive-app.com', GAME_ID = 'c2701133414427fee732e051abdfe3e8', COUNTRY_CODE = 'ww-it' }) {
+/**
+ * This module is intended for dev and testing purpose
+ * 
+ * @export
+ * @param {any} { 
+ *   HOST = 'appsworld.gamifive-app.com', 
+ *   GAME_ID = 'c2701133414427fee732e051abdfe3e8',
+ *   COUNTRY_CODE = 'ww-it',
+ *   HASH = window.location.hash,
+ *   SEARCH = window.location.search,
+ *   PORT = window.location.port,
+ * } 
+ * @returns 
+ */
+export default function location({ 
+  HOST = 'appsworld.gamifive-app.com', 
+  GAME_ID = 'c2701133414427fee732e051abdfe3e8',
+  COUNTRY_CODE = 'ww-it',
+  HASH = window.location.hash,
+  SEARCH = window.location.search,
+  PORT = window.location.port,
+}) {
   return {
-    hash: '',
-    search: '',
-    pathname: `/${COUNTRY_CODE}/html5gameplay/${GAME_ID}/game/sample`,
-    port: '',
+    hash: HASH,
+    search: SEARCH,
+    pathname: `/${COUNTRY_CODE}/html5gameplay/${GAME_ID}/game/sample${SEARCH}${HASH}`,
+    port: PORT,
     hostname: `${HOST}`,
     host: `${HOST}`,
     protocol: 'http:',
     origin: `http://${HOST}`,
-    href: `http://${HOST}/${COUNTRY_CODE}/html5gameplay/${GAME_ID}/game/sample`,
+    href: `http://${HOST}/${COUNTRY_CODE}/html5gameplay/${GAME_ID}/game/sample${SEARCH}${HASH}`,
   };
 }
