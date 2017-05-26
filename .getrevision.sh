3444a280720a553b9066389242388bfa8818760f
eatsjobs <pasqualemangialavori@docomodigital.com>
2017-05-26 17:18:16 +0200

    fix

--- .getrevision.sh
+++ .getrevision.sh
@@ -1,3 +1,3 @@
 GIT_REVISION=$(git describe --tags --long)
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
-echo "module.exports = {\"version\":\"$PACKAGE_VERSION\", \"build\":\"$GIT_REVISION\"};" > src/version.js
+echo -n "module.exports={build:\"$GIT_REVISION\",version:\"$PACKAGE_VERSION\"}" > src/version.js;
