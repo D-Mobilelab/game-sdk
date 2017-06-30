9fb54ace8b820da78d5f75fdfc81f4316c4a49fe
eatsjobs <pasqualemangialavori@docomodigital.com>
2017-06-30 16:58:38 +0200

    GMY-1612 Stringify error in init error action

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -146,7 +146,7 @@ function init(initConfig) {
         })
         .catch((reason) => {
           dispatch({
-            type: 'INIT_ERROR', message: 'INIT_ERROR', initialized: false, initPending: false, error: reason,
+            type: 'INIT_ERROR', message: 'INIT_ERROR', initialized: false, initPending: false, error: JSON.stringify(reason),
           });
         });
   };
