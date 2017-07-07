7e9c0bbb8558bb3760728db0066e62c3d3c8e433
eatsjobs <pasqualemangialavori@docomodigital.com>
2017-07-07 14:58:21 +0200

    fix MenuGamifive

--- src/js/components/Menu/MenuGamifive.jsx
+++ src/js/components/Menu/MenuGamifive.jsx
@@ -2,4 +2,4 @@ import Menu from './Menu';
 import withTheme from '../withTheme';
 import theme from './theme/gamifive.css';
 
-export default withTheme(theme, Menu);
+export default withTheme(Menu, theme);
