c357b242a541da9b61dec8c44bf2087907dcd746
eatsjobs <pasqualemangialavori@docomodigital.com>
2017-07-25 09:51:57 +0200

    debug env for multiple services

--- tools/webpack.config.local.js
+++ tools/webpack.config.local.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 var path = require('path');
 var webpack = require('webpack');
 var baseConfiguration = require('./webpack.config.base');
@@ -21,34 +23,46 @@ var ROOT_DIRECTORY = null;
   devConfiguration.output.publicPath = ROOT_DIRECTORY;
 }*/
 
-var HOSTNAME = 'local.appsworld.gamifive-app.com';
-// var HOSTNAME = '0.0.0.0';
-//devConfiguration.entry.push('webpack/hot/dev-server');
-//devConfiguration.entry.push('webpack-dev-server/client?http://' + HOSTNAME + ':8080');
+var APPSWORLD_SERVICE = 'appsworld.gamifive-app.com';
+var BANDAI_SERVICE = 'bandai2.gameasy.com';
+var GAMEASY_SERVICE = 'www2.gameasy.com';
 
+var PROTOCOL = 'http://';
+var LOCAL = 'local';
+var HOSTNAME = LOCAL + '.' + BANDAI_SERVICE;
+HOSTNAME = '0.0.0.0';
+// devConfiguration.entry.push('webpack/hot/dev-server');
+// devConfiguration.entry.push('webpack-dev-server/client?http://' + HOSTNAME + ':8080');
 
 devConfiguration.devServer = {
   open: true,
   inline: true,
   hot: true,
   contentBase: 'sample/',
-  host: HOSTNAME, // 0.0.0.0 to test on device. then add <ip>:8080/webpack-dev-server/
+  host: HOSTNAME, // 0.0.0.0 to test on device. then add <ip>:8080/
   disableHostCheck: true,
   proxy: {
-    '/v01/**': {
-      target: 'http://appsworld.gamifive-app.com',
+    '/ww-it/v01/**': {
+      onProxyReq: function(proxyReq, req, res) { 
+        console.log('Request:', proxyReq.path);
+        // proxyReq.setHeader('X-Special-Proxy-Header', 'foobar');
+      },
+      onProxyRes: function(proxyRes, req, res) {
+               
+      },
+      target: PROTOCOL + BANDAI_SERVICE,
       secure: false,
       changeOrigin: true,
       historyApiFallback: true,
-      pathRewrite: {
-        '': '',
-      },
-      cookieDomainRewrite: { '*': '' },
+      pathRewrite: function (path, req) { return path.replace('', '') },
+      cookieDomainRewrite: {
+        BANDAI_SERVICE: BANDAI_SERVICE        
+      }
     }
   }
 }
 
-devConfiguration.devtool = 'eval-source-map';
+devConfiguration.devtool = 'eval';
 devConfiguration.plugins.push(envPlugin);
 devConfiguration.plugins.push(hotPlugin);
 
