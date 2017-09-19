22d4bab0be6d3cfbb1e083ad77a2522a8056833d
Pasquale Mangialavori <pasqualemangialavori@docomodigital.com>
2017-09-19 09:48:37 +0200

    fix getrevision on mac

--- .getrevision.sh
+++ .getrevision.sh
@@ -1,3 +1,3 @@
 GIT_REVISION=$(git describe --tags --long)
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
-echo -n "module.exports={build:\"$GIT_REVISION\",version:\"$PACKAGE_VERSION\"}" > src/version.js;
+echo "module.exports={build:\"$GIT_REVISION\",version:\"$PACKAGE_VERSION\"}" > src/version.js;
