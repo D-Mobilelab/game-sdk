ac093c0a5bf02335680a975ca083a6a372634364
Pasquale Mangialavori <pasqualemangialavori@docomodigital.com>
2017-10-24 15:50:28 +0200

    Events mapped to FB pixel events

--- src/js/customMiddleware/track.js
+++ src/js/customMiddleware/track.js
@@ -2,8 +2,18 @@ import NewtonAdapter from 'newton-adapter';
 import FacebookPixelAdapter from 'facebookpixeladapter';
 import FacebookInterface from '../lib/FacebookInterface';
 
+const mapNewtonEventToFacebook = {
+  GameLoad: 'ViewContent',
+  GameStart: 'AddToCart',
+  GameEnd: 'Purchase',
+};
+
 export default function track(eventObject) {
   NewtonAdapter.trackEvent(eventObject);
   FacebookInterface.trackEvent(eventObject.name, eventObject.properties);
-  FacebookPixelAdapter.trackCustom(eventObject.name, eventObject.properties);
+  if (eventObject.name in mapNewtonEventToFacebook) {
+    FacebookPixelAdapter.track(mapNewtonEventToFacebook[eventObject.name], eventObject.properties);
+  } else {
+    FacebookPixelAdapter.trackCustom(eventObject.name, eventObject.properties);
+  }
 }
