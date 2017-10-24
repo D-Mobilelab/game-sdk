ef1648faa77cf49ee5bdf477c64eaf4d13a1742b
Pasquale Mangialavori <pasqualemangialavori@docomodigital.com>
2017-10-24 17:49:22 +0200

    Add right props to facebook events

--- src/js/customMiddleware/track.js
+++ src/js/customMiddleware/track.js
@@ -10,8 +10,12 @@ const mapNewtonEventToFacebook = {
 
 export default function track(eventObject) {
   NewtonAdapter.trackEvent(eventObject);
-  FacebookInterface.trackEvent(eventObject.name, eventObject.properties);
+  // FacebookInterface.trackEvent(eventObject.name, eventObject.properties);
   if (eventObject.name in mapNewtonEventToFacebook) {
+    eventObject.properties.content_ids = [eventObject.properties.label];
+    eventObject.properties.content_name = eventObject.properties.game_title;
+    eventObject.properties.content_type = 'product';
+    eventObject.properties.value = 0;
     FacebookPixelAdapter.track(mapNewtonEventToFacebook[eventObject.name], eventObject.properties);
   } else {
     FacebookPixelAdapter.trackCustom(eventObject.name, eventObject.properties);
