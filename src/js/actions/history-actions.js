a081a37b6afbe136257e38e5a7ab7f46920dfadc
Pasquale Mangialavori <pasqualemangialavori@docomodigital.com>
2017-12-07 09:51:38 +0100

    add: override back button optional

--- src/js/actions/history-actions.js
+++ src/js/actions/history-actions.js
@@ -1,10 +1,12 @@
 import * as HistoryGame from '../lib/HistoryGame';
 import * as menuActions from './menu-actions';
 
-const addressBar = `${window.location.pathname}${window.location.search}`;
-window.history.replaceState({ location: 'step0' }, document.title, `${addressBar}#0`);
-window.history.pushState({ location: 'step1' }, document.title, `${addressBar}#1`);
-window.history.pushState({ location: 'step2' }, document.title, `${addressBar}#2`);
+export function addSteps() {
+  const addressBar = `${window.location.pathname}${window.location.search}`;
+  window.history.replaceState({ location: 'step0' }, document.title, `${addressBar}#0`);
+  window.history.pushState({ location: 'step1' }, document.title, `${addressBar}#1`);
+  window.history.pushState({ location: 'step2' }, document.title, `${addressBar}#2`);
+}
 
 export default function historyHandler(event) {
   return (dispatch) => {
