f5ad8db65a6240d3dc84f74d47c1d01d4792d9ba
gianni <fiorentinig@gmail.com>
2018-07-09 11:52:35 +0200

    check that Raven is available and initialized

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -99,7 +99,9 @@ function init(initConfig) {
         }
       })
       .catch((reason) => {
-        window.Raven.captureException(reason);
+        if (window.Raven && window.Raven.isSetup()) {
+          window.Raven.captureException(reason);
+        }
         dispatch({
           type: 'INIT_ERROR', message: 'INIT_ERROR', initialized: false, initPending: false, reason: reason.toString(),
         });
--- src/store.js
+++ src/store.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === 'development') {
   const logger = createLogger();
   middlewares.push(logger);
 }
-if(window.Raven){
+if(window.Raven.isSetup()){
   middlewares.push(createRavenMiddleware(window.Raven));
 }
 else{
