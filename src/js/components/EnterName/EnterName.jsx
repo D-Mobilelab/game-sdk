06eea6b0f99875e26b2e03f0b357084cd87f59a4
gianni <fiorentinig@gmail.com>
2019-01-22 16:51:10 +0100

    svg close x button

--- src/js/components/EnterName/EnterName.jsx
+++ src/js/components/EnterName/EnterName.jsx
@@ -66,15 +66,15 @@ const CloseX = styled.span`
   position: relative;
   top:  ${props => props.theme.entername.close_x.top};
   right:  ${props => props.theme.entername.close_x.right};
-  /* font-family: Arial,sans-serif; */
   text-align: right;
   font-size: 1.2em;
   display: inline-block;
   color: #ffffff;
   width: 100%;
   display: inline-block;
-  color: ${props => props.theme.entername.close_x.color};
-  margin-bottom: 10px;
+  fill: ${props => props.theme.entername.close_x.color};
+  margin-bottom: 5px;
+  margin-top: 5px;
 `;
 
 const Title = styled.div`
@@ -240,7 +240,11 @@ export class EnterName extends Component {
           <Overlay visible={this.props.show}/>
           <Frame visible={this.props.show}>
             <Container>
-              <CloseX onClick={this.onClose}>&times;</CloseX>
+              <CloseX onClick={this.onClose}>
+              <svg width="20" height="20">
+							  <path d="M15.898,4.045c-0.271-0.272-0.713-0.272-0.986,0l-4.71,4.711L5.493,4.045c-0.272-0.272-0.714-0.272-0.986,0s-0.272,0.714,0,0.986l4.709,4.711l-4.71,4.711c-0.272,0.271-0.272,0.713,0,0.986c0.136,0.136,0.314,0.203,0.492,0.203c0.179,0,0.357-0.067,0.493-0.203l4.711-4.711l4.71,4.711c0.137,0.136,0.314,0.203,0.494,0.203c0.178,0,0.355-0.067,0.492-0.203c0.273-0.273,0.273-0.715,0-0.986l-4.711-4.711l4.711-4.711C16.172,4.759,16.172,4.317,15.898,4.045z"></path>
+						  </svg>
+              </CloseX>
               <Form ref='myForm' onKeyUp={this.onKeyUp} onSubmit={this.onSubmit}>
                 <Title>{this.props.dictionary.WEBAPP_GAMEOVER_INSERT_ALIAS}</Title>
                 <Layout>
--- src/js/components/Leaderboard/Leaderboard.jsx
+++ src/js/components/Leaderboard/Leaderboard.jsx
@@ -78,15 +78,14 @@ const CloseX = styled.span`
   position: relative;
   top:  ${props => props.theme.leaderboard.close_x.top};
   right:  ${props => props.theme.leaderboard.close_x.right};
-  /* font-family: Arial,sans-serif; */
   text-align: right;
-  font-size: 1.2em;
   display: inline-block;
   color: #ffffff;
   width: 100%;
   display: inline-block;
-  color: ${props => props.theme.entername.close_x.color};
-  margin-bottom: 10px;
+  fill: ${props => props.theme.leaderboard.close_x.color};
+  margin-bottom: 5px;
+  margin-top: 5px;
 `;
 
 const ScoreTitle = styled.div`
@@ -272,7 +271,11 @@ export class Leaderboard extends Component {
           <Frame visible={this.props.show}>
             <Container showReplayButton={this.props.showReplayButton}>
               <Top>
-                <CloseX onClick={this.onClose}>&times;</CloseX>
+                <CloseX onClick={this.onClose}>
+                <svg width="20" height="20">
+                  <path d="M15.898,4.045c-0.271-0.272-0.713-0.272-0.986,0l-4.71,4.711L5.493,4.045c-0.272-0.272-0.714-0.272-0.986,0s-0.272,0.714,0,0.986l4.709,4.711l-4.71,4.711c-0.272,0.271-0.272,0.713,0,0.986c0.136,0.136,0.314,0.203,0.492,0.203c0.179,0,0.357-0.067,0.493-0.203l4.711-4.711l4.71,4.711c0.137,0.136,0.314,0.203,0.494,0.203c0.178,0,0.355-0.067,0.492-0.203c0.273-0.273,0.273-0.715,0-0.986l-4.711-4.711l4.711-4.711C16.172,4.759,16.172,4.317,15.898,4.045z"></path>
+                </svg>
+                </CloseX>
                 <ScoreTitle>{this.props.dictionary.WEBAPP_GAMEOVER_YOUR_SCORE}</ScoreTitle>
                 <ScoreContainer>
                   <ArrowLeft/>
@@ -284,8 +287,6 @@ export class Leaderboard extends Component {
 
                   {this.props.positions.map((position, i) => {
                   return <ScoreListRow><Position>{i + 1}{this.props.dictionary['WEBAPP_GAMEOVER_POSITION_' + (i + 1)]}</Position><Score>{position.score}</Score><Name>{position.player_name}</Name></ScoreListRow>;
-                  // return <ScoreListRow><Position>{i+1}{(i===0)?'st':(i===1)?'nd':(i===2)?'rd':'th'}</Position><Score>{position.score}</Score><Name>{position.player_name}</Name></ScoreListRow>;
-                  return <ObjectRow obj={object} key={i} />;
               })}
 
                 </HighScoreList>
