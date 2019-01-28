8ded207bdbbd2496ad82b57eeeaf59ad028d3d17
gianni <fiorentinig@gmail.com>
2019-01-28 16:02:37 +0100

    removed components:  related and  menu button

--- src/js/components/Menuold/MenuBandai.jsx
+++
@@ -1,5 +0,0 @@
-import Menu from './Menu';
-import withTheme from '../withTheme';
-import theme from './theme/bandai.css';
-
-export default withTheme(Menu, theme);
--- src/js/components/Menuold/MenuGamempire.jsx
+++
@@ -1,5 +0,0 @@
-import Menu from './Menu';
-import withTheme from '../withTheme';
-import theme from './theme/gamempire.css';
-
-export default withTheme(Menu, theme);
